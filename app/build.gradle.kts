ef3b8043c616573c364ca3616e856f6bfa002e44
ristonrodrigues723 <maximusimperiousthe723rd@gmail.com>
2024-07-12 11:17:20 +0530

    sixth commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,18 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+
+}
+
+fun getLocalProperty(key: String, defaultValue: String): String {
+    val properties = Properties()
+    val localProperties = rootProject.file("local.properties")
+    if (localProperties.exists()) {
+        properties.load(FileInputStream(localProperties))
+    }
+    return properties.getProperty(key, defaultValue)
 }
 
 android {
@@ -14,34 +27,35 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "GEMINI_API_KEY", "\"${getLocalProperty("GEMINI_API_KEY", "")}\"")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        buildConfig = true
+    }
+
 }
-
-
-
 dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat.v170)
+    implementation(libs.material.v1110)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    implementation(libs.okhttp)
-
+    androidTestImplementation(libs.junit.v121)
+    androidTestImplementation(libs.espresso.core.v361)
+    implementation("org.apache.httpcomponents:httpclient:4.5.14") {
+        exclude(group = "org.apache.httpcomponents", module = "httpcore")
+    }
+    implementation(libs.google.cloud.aiplatform)
 }
